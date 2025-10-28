15ed1bed3bf8373494c3f2b9a99c78f3c0a1bb42
Nick-Major <divanchik26@gmail.com>
2025-10-28 17:13:12 +0300

    Routes are sorted

--- routes/web.php
+++ routes/web.php
@@ -12,43 +12,49 @@ use Illuminate\Support\Facades\Route;
 // Главная страница
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-// Аутентификация (только для администраторов)
+// Аутентификация
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// Публичные маршруты для гостей
+// Фильмы
 Route::get('/movies', [MovieController::class, 'withActiveSessions'])->name('movies.index');
 Route::get('/movies/{movie}', [MovieController::class, 'show'])->name('movies.show');
+
+// Сеансы
 Route::get('/sessions', [MovieSessionController::class, 'listSessions'])->name('sessions.index');
 Route::get('/sessions/{session}', [MovieSessionController::class, 'show'])->name('sessions.show');
 Route::get('/sessions/{session}/seats', [MovieSessionController::class, 'availableSeats'])->name('sessions.seats');
 
-// Бронирование билетов (доступно гостям без авторизации)
-Route::post('/tickets/book', [TicketController::class, 'bookTicket'])->name('tickets.book');
+// Билеты и бронирование
 Route::get('/sessions/{session}/booking', [TicketController::class, 'showBookingPage'])->name('sessions.booking');
+Route::post('/tickets/book', [TicketController::class, 'bookTicket'])->name('tickets.book');
 Route::get('/tickets/{ticket}/confirmation', [TicketController::class, 'showConfirmation'])->name('tickets.confirmation');
 Route::get('/tickets/{code}', [TicketController::class, 'showTicket'])->name('tickets.show');
-
-// API маршруты
 Route::post('/tickets/check-availability', [TicketController::class, 'checkSeatAvailability'])->name('tickets.check-availability');
 
-// Админка (требует авторизации и проверки прав администратора)
+// Админка
 Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
+    // Дашборд
     Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
     
-    // Ресурсы админки
+    // Основные CRUD ресурсы
     Route::resource('halls', CinemaHallController::class);
     Route::resource('movies', MovieController::class);
     Route::resource('sessions', MovieSessionController::class);
 
+    // Управление залами
     Route::get('/halls/{hall}/configuration', [CinemaHallController::class, 'configuration'])->name('halls.configuration');
     Route::get('/halls/{hall}/prices', [CinemaHallController::class, 'prices'])->name('halls.prices');
     Route::post('/halls/{hall}/generate-layout', [CinemaHallController::class, 'generateLayout'])->name('halls.generate-layout');
     Route::post('/halls/{hall}/save-configuration', [CinemaHallController::class, 'saveConfiguration'])->name('halls.save-configuration');
     Route::post('/halls/{hall}/update-prices', [CinemaHallController::class, 'updatePrices'])->name('halls.update-prices');
-    Route::post('/movies/{movie}/toggle-active', [MovieController::class, 'toggleActive'])->name('movies.toggle-active');
+
+    // Управление фильмами
     Route::get('/movies/{movie}/edit', [MovieController::class, 'edit'])->name('movies.edit');
+    Route::post('/movies/{movie}/toggle-active', [MovieController::class, 'toggleActive'])->name('movies.toggle-active');
+
+    // Управление сеансами
     Route::get('/sessions/{movieSession}/edit', [MovieSessionController::class, 'edit'])->name('sessions.edit');
     Route::post('/sessions/{movieSession}/toggle-actual', [MovieSessionController::class, 'toggleActual'])->name('sessions.toggle-actual');
     Route::post('/sessions/cleanup', [MovieSessionController::class, 'cleanupOldSessions'])->name('sessions.cleanup');
